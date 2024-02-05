a8718cd679f2c308c9529a5ceff2d7327af74fd4
2112531 <2112531@m365.tarc.edu.my>
2024-02-05 16:18:41 +0800

    minor change

---
+++ CPE-Platform/Private/StaffRewardsAllocation.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CPE_Platform.Private
+{
+	public partial class StaffRewardsAllocation : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
