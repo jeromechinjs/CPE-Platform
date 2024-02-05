448b6115bf2dd362468df1582285dd8a74280029
2112531 <2112531@m365.tarc.edu.my>
2024-02-05 19:09:13 +0800

    later continue with staff rewards allocation

--- CPE-Platform/Private/StaffRewardsAllocation.aspx
+++ CPE-Platform/Private/StaffRewardsAllocation.aspx
@@ -1,15 +1,28 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StaffRewardsAllocation.aspx.cs" Inherits="CPE_Platform.Private.StaffRewardsAllocation" %>
+﻿<%@ Page Title="Staff Rewards Allocation" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="StaffRewardsAllocation.aspx.cs" Inherits="CPE_Platform.Private.StaffRewardsAllocation" %>
 
-<!DOCTYPE html>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+	<!DOCTYPE html>
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<script type="text/javascript">
+			$(function () {
+				$('[id*=lstStudent]').multiselect({
+					includeSelectAllOption: true,
+					templates: {
+						button: '<button type="button" class="multiselect dropdown-toggle" data-bs-toggle="dropdown"><span class="multiselect-selected-text"></span></button>'
+					}
+				});
+			});
+		</script>
+		<style>
+			
+		</style>
+	</head>
+	</html>
+	<asp:DropDownList ID="CPECourse_DropDown" runat="server" class="form-select" Width="300px" AutoPostBack="true" OnSelectedIndexChanged="CPECourse_DropDown_SelectedIndexChanged">
+		<asp:ListItem Value="0">Select Course</asp:ListItem>
+	</asp:DropDownList>
+	<asp:ListBox ID="lstStudent" runat="server" SelectionMode="Multiple" AutoPostBack="true" ></asp:ListBox>
+	<asp:Button Text="Submit" runat="server" OnClick="Submit" />
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-        <div>
-        </div>
-    </form>
-</body>
-</html>
+</asp:Content>
--- CPE-Platform/Private/StaffRewardsAllocation.aspx.cs
+++ CPE-Platform/Private/StaffRewardsAllocation.aspx.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +15,85 @@ namespace CPE_Platform.Private
 {
 	public partial class StaffRewardsAllocation : System.Web.UI.Page
 	{
+		SqlCommand cmd;
+		SqlDataAdapter dataAdapter;
+		DataSet ds = new DataSet();
+		string query;
 		protected void Page_Load(object sender, EventArgs e)
 		{
+			if(!IsPostBack)
+			{
+				GetCPEName();
+				lstStudent.Items.Insert(0, " Select Students");
+			}
+		}
+		private void GetCPEName()
+		{
+			SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+			query = "Select * from CPE_Course";
+			dataAdapter = new SqlDataAdapter(query, con);
 
+			dataAdapter.Fill(ds);
+
+			if (ds.Tables[0].Rows.Count > 0)
+			{
+				CPECourse_DropDown.DataSource = ds;
+				CPECourse_DropDown.DataTextField = "CPEDesc";
+				CPECourse_DropDown.DataValueField = "CPECode";
+				CPECourse_DropDown.DataBind();
+				CPECourse_DropDown.Items.Insert(0, new ListItem("Any Course", "0"));
+			}
+		}
+		protected void Submit(object sender, EventArgs e)
+		{
+			string message = "";
+			foreach (ListItem item in lstStudent.Items)
+			{
+				if (item.Selected)
+				{
+					message += item.Text + " " + item.Value + "\\n";
+				}
+			}
+			ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('" + message + "');", true);
+		}
+
+		
+
+		protected void CPECourse_DropDown_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string get_CPECode, get_CPEName;
+
+			get_CPECode = CPECourse_DropDown.SelectedValue.ToString();
+			get_CPEName = CPECourse_DropDown.SelectedItem.Text;
+
+			SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+			// to fill up the drop down based on the db data
+			if (get_CPECode != "0")
+			{
+				query = "SELECT R.StudentID, S.StudentName from CPE_Registration R, Student S WHERE CPECode ='" + get_CPECode.ToString() + "' AND R.StudentID = S.StudentID";
+				dataAdapter = new SqlDataAdapter(query, con);
+				dataAdapter.Fill(ds);
+				if (ds.Tables[0].Rows.Count > 0)
+				{
+					lstStudent.DataSource = ds;
+					lstStudent.DataTextField = "StudentName";
+					lstStudent.DataValueField = "StudentID";
+					lstStudent.DataBind();
+					//lstStudent.Items.Insert(0, new ListItem("Choose Students from" + get_CPEName.ToString()));
+					lstStudent.SelectedIndex = 0;
+				}
+				else
+				{
+					lstStudent.Items.Insert(0, " There are no student completed " + get_CPEName.ToString());
+				}
+
+			}
+			else
+			{
+				lstStudent.Items.Clear();
+				lstStudent.Items.Insert(0, " Select Students");
+
+			}
 		}
 	}
 }
